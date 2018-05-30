13c354846f662538a25edc44316ebb6ee1f4721a
Anna Miller <annaoberlandermiller@gmail.com>
2018-05-30 16:32:45 -0400

    Commiting Pig Dice Game

--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -8,8 +8,41 @@ namespace PigDice
 {
 	class Program
 	{
+
+		Random rnd = new Random();//this is available to all methods, because it's inside of the class, but not in a specific method
+
+		int RollDie()
+		{
+			return rnd.Next(1, 7);
+		}
+
+		void PlayPigDice()
+		{
+			int score = 0;
+			int die = RollDie();
+			while(die != 1) {
+				score += die;
+				die = RollDie();
+			}
+			Console.WriteLine("Score {0}", score);
+		}
+
+		void Run() {
+			bool quit = false;
+			while (!quit) {  //! means not - so this is saying not false(same as true), since we defined quit as false
+				PlayPigDice();
+
+				Console.Write("Do you want to quit?");
+				string answer = Console.ReadLine();
+				answer = answer.ToUpper();  //this is changing the response to uppercase
+				quit = answer.StartsWith("Y");
+			}
+				
+		}
+
 		static void Main(string[] args)
 		{
+			(new Program()).Run(); //this calls the run method on line 10
 		}
 	}
 }
